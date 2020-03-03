75f37831954a0dec9f8436de810364b4187a9d5f
lihang <lihang@jiuyescm.com>
2020-03-03 11:39:10 +0800

    fix:settings

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -103,7 +103,8 @@ DATABASES = {
         'PASSWORD': "123456",
         'HOST': "127.0.0.1",
         'PORT': 3306,
-        'OPTIONS': {'charset': 'utf8mb4'},
+        'OPTIONS': {'charset': 'utf8mb4',
+        "init_command": "SET foreign_key_checks = 0;"},
     }
 }
 
